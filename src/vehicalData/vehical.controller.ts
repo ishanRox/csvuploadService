2be22ca5f7dadc5886d16b19032133919b7a3af5
ishanRox <roxishan8@gmail.com>
2021-04-07 15:20:34 +0530

    removed unwanted routes (put delete)

--- src/vehicalData/vehical.controller.ts
+++ src/vehicalData/vehical.controller.ts
@@ -8,13 +8,8 @@ export class vehicalController {
     private logger = new Logger('IdeaController');
     constructor(private vehicalService: VehicalService) { }
 
-    @Get()
-    showAllIdeas() {
-        return this.vehicalService.showAll();
-    }
-
-    //this is the major one others optional
-    //just created for testing
+     //this is the major one others optional
+    //other routes just created for testing
     @Post()
     createVehical(@Body() data: VehicalDTO) {
 
@@ -22,23 +17,4 @@ export class vehicalController {
         return this.vehicalService.create(data)
     }
 
-
-
-
-    @Get(':id')
-    readIdea(@Param('id') id: string) {
-
-        return this.vehicalService.read(id);
-    }
-
-    @Put(':id')
-    updateIdea(@Param('id') id: string, @Body() data: Partial<VehicalDTO>) {
-        this.logger.log(JSON.stringify(data));
-        return this.vehicalService.update(id, data);
-    }
-
-    @Delete(':id')
-    destroyIdea(@Param('id') id: string) {
-        return this.vehicalService.destroy(id);
-    }
 }
